9df8827efd225ce3b9bbb51a2521f64fe9e64fa7
Ludger Solbach <soulman@soulspace.org>
2013-07-19 11:56:04 +0200

    updated dependencies

--- module.clj
+++ module.clj
@@ -7,5 +7,5 @@
  :plugins ["global" "deps" "clojure" "package"]
  :dependencies [["org.clojure" "clojure" "1.5.1"]
                 ["org.clojure" "data.xml" "0.0.7"]
-                ["org.soulspace.clj" "CljXmlLibrary" "0.1.0"]]
+                ["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]
  ]
