d7f06025d0f05ae94f76749c2a879d6070474512
Ludger Solbach <soulman@soulspace.org>
2013-07-26 03:40:26 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,10 +1,10 @@
 [
- :name "CljXmlLibrary"
+ :module "CljXmlLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.2.0"
  :description "The CljXmlLibrary is a library for building XML DSLs in clojure"
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.5.1"]
-                ["org.clojure" "data.xml" "0.0.7"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.clojure" "data.xml" "0.0.7"]]]
  ]
