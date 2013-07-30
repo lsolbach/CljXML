449ba0c2030a4cbd8416ab39e119ad42b0b3b0bd
Ludger Solbach <soulman@soulspace.org>
2013-07-30 19:55:59 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,11 +1,11 @@
 [
- :name "CljDocbookLibrary"
+ :module "CljDocbookLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.1.0"
  :description "The CljDocbookLibrary provides a DSL for the programmatic generation of Docbook xml files."
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.5.1"]
-                ["org.clojure" "data.xml" "0.0.7"]
-                ["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.clojure" "data.xml" "0.0.7"]]
+                [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
  ]
