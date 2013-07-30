1a6b28b045901c53a9dea189162d092b3c564580
Ludger Solbach <soulman@soulspace.org>
2013-07-30 20:49:12 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,11 +1,11 @@
 [
- :name "CljXslLibrary"
+ :module "CljXslLibrary"
  :project "org.soulspace.clj"
- :type "library"
+ :type :library
  :version "0.1.0"
  :description "The CljXslLibrary provides a DSL for the programmatic generation of XSL files."
- :plugins ["global" "deps" "clojure" "package"]
- :dependencies [["org.clojure" "clojure" "1.5.1"]
-                ["org.clojure" "data.xml" "0.0.7"]
-                ["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]
+ :plugins ["global" "sdeps" "clojure" "package"]
+ :dependencies [[["org.clojure" "clojure" "1.5.1"]]
+                [["org.clojure" "data.xml" "0.0.7"]]
+                [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
  ]
