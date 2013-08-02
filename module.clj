2939692f9407f2d8d90d1849a690f99ad1273066
Ludger Solbach <soulman@soulspace.org>
2013-08-02 18:35:55 +0200

    updated module files

--- module.clj
+++ module.clj
@@ -4,8 +4,7 @@
  :type :library
  :version "0.1.0"
  :description "The CljBpmnLibrary provides a DSL for the programmatic generation of BPMN 2.0 xml definitions."
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.clojure" "data.xml" "0.0.7"]]
                 [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
  ]
