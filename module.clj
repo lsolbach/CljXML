43fe88f317e8ba92477b03c8235880b84daa7b73
Ludger Solbach <soulman@soulspace.org>
2013-08-03 07:09:48 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :library
  :version "0.2.0"
  :description "The CljXmlLibrary is a library for building XML DSLs in clojure"
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure"  "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["org.clojure" "data.xml" "0.0.7"]]]
  ]
