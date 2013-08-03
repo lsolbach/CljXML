5e27f726cc49c24428a4729cbcb7e9e053dce240
Ludger Solbach <soulman@soulspace.org>
2013-08-03 20:35:58 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :library
  :version "0.1.0"
  :description "The CljFoLibrary provides a DSL for the generation of XSL Formatting Objects (XSL-FO) files."
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
                 [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
  ]
