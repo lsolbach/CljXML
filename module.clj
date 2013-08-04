a9b05a81026797a692f7879d1c09ca380b422627
Ludger Solbach <soulman@soulspace.org>
2013-08-04 06:15:53 +0200

    bumped version to 0.3.0, added ability to create xml without namespaces

--- module.clj
+++ module.clj
@@ -2,7 +2,7 @@
  :module "CljXmlLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.2.0"
+ :version "0.3.0"
  :description "The CljXmlLibrary is a library for building XML DSLs in clojure"
  :plugins ["global" "dependencies" "clojure"  "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
--- src/org/soulspace/clj/xml/dsl_builder.clj
+++ src/org/soulspace/clj/xml/dsl_builder.clj
@@ -10,19 +10,30 @@
   ;(str/replace (.toLowerCase tag) \. \-)
   )
 
-(defmacro deftag [ns-prefix tag & [attrs]]
-  "define a function for the given tag that generates the xml representation."
-  (let [xml-fn-name (symbol (fn-name tag))]
-    `(defn ~xml-fn-name [& [attrs# & content#]]
-       (element (keyword (str ~ns-prefix ":" ~tag))
-                (merge (or ~attrs {}) (or attrs# {}))
-                (remove #(or (nil? %) (empty? %)) content#)))))
+(defmacro deftag 
+  "Define a function for the given tag that generates the xml representation."
+  ([tag]
+    (let [xml-fn-name (symbol (fn-name tag))]
+      `(defn ~xml-fn-name [& [attrs# & content#]]
+         (element (keyword ~tag)
+                  (or attrs# {})
+                  (remove #(or (nil? %) (empty? %)) content#)))))
+  ([ns-prefix tag & [attrs]]
+    (let [xml-fn-name (symbol (fn-name tag))]
+      `(defn ~xml-fn-name [& [attrs# & content#]]
+         (element (keyword (str ~ns-prefix ":" ~tag))
+                  (merge (or ~attrs {}) (or attrs# {}))
+                  (remove #(or (nil? %) (empty? %)) content#))))))
+ 
+(defmacro deftags
+  "Defines functions for the given tags that generate the xml representation per tag"
+  ([tags]
+    `(do ~@(map (fn [tag] `(deftag ~tag)) tags)))
+  ([ns-prefix tags]
+    `(do ~@(map (fn [tag] `(deftag ~ns-prefix ~tag)) tags))))
 
-(defmacro defroottags [ns-prefix ns-uri root-tags]
-  "defines functions for the given tags that generate the xml representations per tag including the namespace declaration"
+(defmacro defroottags
+  "Defines functions for the given tags that generate the xml representations per tag including the namespace declaration"
+  [ns-prefix ns-uri root-tags]
   `(do ~@(map (fn [tag] `(deftag ~ns-prefix ~tag {(keyword (str "xmlns:" ~ns-prefix)) ~ns-uri})) root-tags)))
-
-(defmacro deftags [ns-prefix tags]
-  "defines functions for the given tags that generate the xml representation per tag"
-  `(do ~@(map (fn [tag] `(deftag ~ns-prefix ~tag)) tags)))
 
