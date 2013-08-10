13ed20b0bef0eeab14694f0a6437929b2c4c6ff9
Ludger Solbach <soulman@soulspace.org>
2013-08-10 06:42:01 +0200

    bumped version to 0.2.0, updated module.clj

--- module.clj
+++ module.clj
@@ -2,9 +2,9 @@
  :module "CljXHtmlLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.0"
+ :version "0.2.0"
  :description "The CljXHtmlLibrary provides a DSL for the programmatic generation of XHtml files."
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
- :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]]
  ]
