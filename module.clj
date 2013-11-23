559163755bb140766800fb6261922349feeebb49
Ludger Solbach <soulman@soulspace.org>
2013-11-23 12:34:04 +0100

    updated module files

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :type :library
  :version "0.1.0"
  :description "The CljXsdLibrary provides a DSL for the programmatic generation of XML Schema files"
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
+                [["org.soulspace.clj" "CljXmlLibrary" "0.3.0"]]]
  ]
