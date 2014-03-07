ac5665f61604266103573d346e27447314f43915
Ludger Solbach <soulman@soulspace.org>
2014-03-07 09:02:05 +0100

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,11 @@
  :type :library
  :version "0.1.0"
  :description "The CljMathmlLibrary provides a DSL for the programmatic generation of MathML XML files."
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
- :dependencies [[["org.clojure" "clojure" "1.5.1"]]
-                [["org.soulspace.clj" "CljXmlLibrary" "0.2.0"]]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/ClojureTestPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.4.0"]]
  ]
