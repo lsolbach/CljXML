b091426b2b66ca4b023fdd812871340a5b060863
Ludger Solbach <soulman@soulspace.org>
2015-11-13 11:03:08 +0100

    added some code for toying with metadata on collections

---
+++ CljXmlLibrary/test/org/soulspace/clj/xml/macrotest.clj
@@ -0,0 +1,92 @@
+(ns org.soulspace.clj.xml.macrotest
+  (:use [org.soulspace.clj.xml marshalling zip])
+  (:require [clojure.zip :as zip]
+            [clojure.data.xml :as xml]))
+
+(defmacro metatest [v]
+  (doseq [entry v]
+    (println "Element:")
+    (println entry (meta entry))
+    (when (:attributes entry)
+      (doseq [a (:attributes entry)]
+        (println "  Attribute")
+        (println "  " a (meta a))))
+    (when (:content entry)
+      (doseq [c (:content entry)]
+        (println "  Content")
+        (println "  " c (meta c))))))
+
+; hierarchical map
+(def hierarchical-spec
+ ^{:ns "http://ecipse.org/classpath"}
+  ^{:ns-prefix "cp"}
+  {:element "Classpath"
+   :content
+   [^:optional
+    ^:many
+    {:element "Classpathentry"
+     :attributes ["kind" "path"]
+     :content
+     [^:optional
+      {:element "Attributes"
+       :content 
+       [^:optional
+        ^:many
+        {:element "Attribute"
+         :attributes ["name" "value"]}]}]}]}
+)
+; flat vector of  maps
+(def flat-spec
+  ^{:ns-prefix "cp"}
+  '[
+    ^:root
+    ^{:ns "http://ecipse.org/classpath"}
+    {:element Classpath
+     :content
+     [^:optional
+      ^:many
+      Classpathentry]}
+    
+    {:element Classpathentry
+     :attributes ["kind" "path"]
+     :content
+     [^:optional
+      Attributes]}
+    
+    {:element Attributes
+     :content 
+     [^:optional
+      ^:many
+      Attribute]}
+    
+    {:element Attribute
+     :attributes ["name" "value"]}
+    ]
+  )
+
+(metatest ^{:ns-prefix "cp"}
+  [
+   ^:root
+   ^{:ns "http://ecipse.org/classpath"}
+   {:element Classpath
+    :content
+    [^:optional
+     ^:many
+     Classpathentry]}
+   
+   {:element Classpathentry
+    :attributes ["kind" "path"]
+    :content
+    [^:optional
+     Attributes]}
+   
+   {:element Attributes
+    :content 
+    [^:optional
+     ^:many
+     Attribute]}
+   
+   {:element Attribute
+    :attributes ["name" "value"]}
+   ])
+
