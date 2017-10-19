8587e5c569d7276301f6327bebe674cdaa5202cc
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-19 10:26:03 +0200

    updated dependency on CljXmlLibrary

--- CljXmlLibrary/module.clj
+++ CljXmlLibrary/module.clj
@@ -2,12 +2,12 @@
  :module "CljXmlLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.4.2"
+ :version "0.4.3"
  :description "The CljXmlLibrary is a library for building XML DSLs in Clojure"
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
                 ["org.clojure/data.xml, 0.0.7"]
-                ["org.soulspace.clj/CljLibrary, 0.6.1"]]
+                ["org.soulspace.clj/CljLibrary, 0.7.0"]]
  ]
