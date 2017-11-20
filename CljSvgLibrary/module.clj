d8c82c9ef7e32549c040560278076c2604c16ec3
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-11-20 16:22:09 +0100

    updated batik dependencies, bumped version to 0.1.3

--- CljSvgLibrary/module.clj
+++ CljSvgLibrary/module.clj
@@ -2,14 +2,15 @@
  :module "CljSvgLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.1.2"
+ :version "0.1.3"
  :description "The CljSvgLibrary provides a DSL for the programmatic generation of SVG files and wrapper functions for Apache Batik."
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.8.0"]
-                ["org.apache.xmlgraphics/batik-swing, 1.7"]
-                ["org.apache.xmlgraphics/batik-svggen, 1.7"]
+                ["org.apache.xmlgraphics/batik-swing, 1.9.1"]
+                ["org.apache.xmlgraphics/batik-svggen, 1.9.1"]
+                ["org.apache.xmlgraphics/batik-svg-dom, 1.9.1"]
                 ["org.soulspace.clj/CljXmlLibrary, 0.4.3"]
                 ["org.soulspace.clj/CljJavaLibrary, 0.7.0"]]
  ]
--- CljSvgLibrary/project.clj
+++ CljSvgLibrary/project.clj
@@ -1,11 +1,12 @@
-(defproject org.soulspace.clj/CljSvgLibrary "0.1.2"
+(defproject org.soulspace.clj/CljSvgLibrary "0.1.3"
   :description "The CljSvgLibrary provides a DSL for the programmatic generation of MathML XML files"
   :url "https://github.com/lsolbach/CljXML"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.xmlgraphics/batik-swing "1.7"]
-                 [org.apache.xmlgraphics/batik-svggen "1.7"]
+                 [org.apache.xmlgraphics/batik-swing "1.9.1"]
+                 [org.apache.xmlgraphics/batik-svggen "1.9.1"]
+                 [org.apache.xmlgraphics/batik-svg-dom "1.9.1"]
                  [org.soulspace.clj/CljXmlLibrary "0.4.3"]
                  [org.soulspace.clj/CljJavaLibrary "0.7.0"]]
   :test-paths ["unittest"])
