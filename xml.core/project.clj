2bbe9e2f89236c98441e61fb57609cfdc0d65138
Ludger Solbach <soulman@soulspace.org>
2020-07-27 01:19:48 +0200

    updated dependency

--- xml.core/project.clj
+++ xml.core/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.xml "0.0.7"]
+                 [org.clojure/data.xml "0.0.8"]
                  [org.soulspace.clj/clj.base "0.8.0"]]
   :test-paths ["unittest"])
