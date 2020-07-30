b30a2f4c670aae85d91b5ab1c3deed4c4d8aede1
Ludger Solbach <soulman@soulspace.org>
2020-07-30 22:55:40 +0200

    enhanced documentation

--- xml.dsl/src/org/soulspace/xml/dsl/builder.clj
+++ xml.dsl/src/org/soulspace/xml/dsl/builder.clj
@@ -9,14 +9,13 @@
 ;;
 
 (ns org.soulspace.xml.dsl.builder
+  "Contains macros to build a DSL for a XML dialact."
   (:require [clojure.data.xml :as xml]
             [clojure.string :as str]
             [org.soulspace.clj.string :as sstr]))
 
-
-; TODO generate 'nice' names for tags like RDF, bagID and nodeID 
 (defn- fn-name [tag]
-  "Converts tag to valid function name"
+  "Converts the 'tag' to a valid function name"
   (str/lower-case (sstr/to-kebab-case tag)))
 
 ; TODO use optional named argument for attr map
@@ -42,13 +41,17 @@
                  (remove #(or (nil? %) (empty? %)) content#))))))
 
 (defmacro deftags
-  "Defines functions for the given tags that generate the xml representation per tag"
+  "Defines functions for the given 'tags' that generate the XML representation per tag.
+  Optionally takes a namespace prefix as 'ns-prefix', which is prepended to the generated tags."
   ([tags]
    `(do ~@(map (fn [tag] `(deftag ~tag)) tags)))
   ([ns-prefix tags]
    `(do ~@(map (fn [tag] `(deftag ~ns-prefix ~tag)) tags))))
 
 (defmacro defroottags
-  "Defines functions for the given tags that generate the xml representations per tag including the namespace declaration"
+  "Defines functions for the given 'root-tags' that generate the XML representations per tag.
+  The macro needs a namespace prefix as 'ns-prefix' and a namespace URI as 'ns-uri'
+  to generate a namespace declaration for a root tag. If no namespace declaration is wanted,
+  just add the root tag to the collection passed todeftags."
   [ns-prefix ns-uri root-tags]
   `(do ~@(map (fn [tag] `(deftag ~ns-prefix ~tag {(keyword (str "xmlns:" ~ns-prefix)) ~ns-uri})) root-tags)))
