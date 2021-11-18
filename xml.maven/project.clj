45f90ece3d3b94b1e56c67aa7fa28d5f8fc3d80c
Ludger Solbach <soulman@soulspace.org>
2021-11-18 11:12:53 +0100

    added deps.edn, updated project.clj

--- xml.maven/project.clj
+++ xml.maven/project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/lsolbach/CljDevTools"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [org.soulspace.clj/clj.base "0.8.0"]
-                 [org.soulspace.clj/xml.core "0.5.0"]
-                 [org.soulspace.clj/xml.dsl "0.5.0"]]
-  :test-paths ["unittest"])
+
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
+  :test-paths ["test"]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
