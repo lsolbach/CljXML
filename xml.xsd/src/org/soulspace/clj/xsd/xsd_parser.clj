46adfd36c0ef346b43c1aff7c1a0130e2e64ae9d
Ludger Solbach <soulman@soulspace.org>
2021-12-09 09:58:22 +0100

    added xsd parser structure and deps.edn

---
+++ xml.xsd/src/org/soulspace/clj/xsd/xsd_parser.clj
@@ -0,0 +1,145 @@
+(ns org.soulspace.clj.xsd.xsd-parser
+  (:require [clojure.data.xml :as xml]))
+
+
+(defn parse-documentation
+  "Parses an XML schema documentation."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-annotation
+  "Parses an XML schema annotation."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-annotations
+  "Parses an XML schema annotations."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-field
+  "Parses an XML schema field."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-selector
+  "Parses an XML schema selector."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-key
+  "Parses an XML schema key."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-enumeration
+  "Parses an XML schema enumeration."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-any-attribute
+  "Parses an XML schema any attribute."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-attribute
+  "Parses an XML schema attribute."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-attribute-group
+  "Parses an XML schema attribute group."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-element
+  "Parses an XML schema element."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-list
+  "Parses an XML schema list."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-union
+  "Parses an XML schema union."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-sequence
+  "Parses an XML schema sequence."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-choice
+  "Parses an XML schema choice."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-group
+  "Parses an XML schema group."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-extension
+  "Parses an XML schema extension."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-restriction
+  "Parses an XML schema restriction."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-simple-type
+  "Parses an XML schema simple type."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-complex-content
+  "Parses an XML schema complex content."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-complex-type
+  "Parses an XML schema complex type."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-import
+  "Parses an XML schema import."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(defn parse-schema
+  "Parses an XML schema document."
+  [{tag :tag attrs :attrs content :content}]
+;
+  )
+
+(comment
+  ;
+  )
